6ce71183bc64199950ea9c6d733ef2cb8d1758ae
cristhian <cristhian.torres@mail.escuelaing.edu.co>
2021-02-27 09:41:34 -0500

    Parte 2 hasta punto 10

---
+++ servlet/src/main/java/edu/eci/cvds/servlet/Clase.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.servlet;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Optional;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ *
+ * @author Lenovo
+ */
+public class Clase extends HttpServlet{
+   
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   }
+   
+}
---
+++ servlet/src/main/java/edu/eci/cvds/servlet/model/Todo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.servlet.model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Todo {
+    
+    public Todo(){
+    
+    }
+
+    public void getter(){
+        
+    }
+    
+    public void setter(){
+        
+    }
+}
+
